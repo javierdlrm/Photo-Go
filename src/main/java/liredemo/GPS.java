77ad7a92bc158ae31d122f72e4639f8ff1587a6c
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2019-03-19 12:08:34 +0100

    Last changes

--- src/main/java/liredemo/GPS.java
+++ src/main/java/liredemo/GPS.java
@@ -1,10 +1,13 @@
 package liredemo;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,10 +18,12 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.lang.GeoLocation;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.GpsDirectory;
+
+import net.semanticmetadata.lire.imageanalysis.features.GlobalFeature;
 import net.semanticmetadata.lire.imageanalysis.features.LireFeature;
 import net.semanticmetadata.lire.utils.SerializationUtils;
 
-public class GPS implements net.semanticmetadata.lire.imageanalysis.features.GlobalFeature {
+public class GPS implements GlobalFeature {
 
     private double[] coordinates;
     final private String separator = ":";
@@ -96,25 +101,32 @@ public class GPS implements net.semanticmetadata.lire.imageanalysis.features.Glo
 
     @Override
     public void extract(BufferedImage image) {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        Metadata metadata;
         try {
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            Metadata metadata;
+
             System.out.println("[GPS] Extracting...");
-            ImageIO.write(image, "png", output);
-            metadata = ImageMetadataReader
-                    .readMetadata(new ByteArrayInputStream(output.toByteArray(), 0, output.size()));
-            final GpsDirectory gps = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+            if (ImageIO.write(image, "jpg", output)) {
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(output.toByteArray(), 0, output.size());
+                metadata = ImageMetadataReader.readMetadata(inputStream);
+                final Collection<GpsDirectory> gpss = metadata.getDirectoriesOfType(GpsDirectory.class);
 
-            if (gps == null) {
+                if (gpss == null || gpss.size() == 0) {
+                    System.out.println("[GPS] Tags not found");
+                    this.coordinates = new double[] { 0, 0 };
+                } else {
+
+                    for (GpsDirectory gps : gpss) {
+                        System.out.println("[GPS] Getting location...");
+                        GeoLocation location = gps.getGeoLocation();
+                        this.coordinates = new double[] { location.getLatitude(), location.getLongitude() };
+                        System.out.println("[GPS] Latitude: " + coordinates[0] + ", Longitude: " + coordinates[1]);
+                    }
+                }
+            } else {
                 System.out.println("[GPS] Tags not found");
-                this.coordinates = null;
-                return;
+                this.coordinates = new double[] { 0, 0 };
             }
-
-            System.out.println("[GPS] Getting location...");
-            GeoLocation location = gps.getGeoLocation();
-            this.coordinates = new double[] { location.getLatitude(), location.getLongitude() };
-
             System.out.println("[GPS] Latitude: " + coordinates[0] + ", Longitude: " + coordinates[1]);
             System.out.println("[GPS] Double[]: ");
             System.out.println(SerializationUtils.toString(this.coordinates));
@@ -126,4 +138,41 @@ public class GPS implements net.semanticmetadata.lire.imageanalysis.features.Glo
         }
     }
 
+    // private void nacti(final ByteArrayInputStream istm) throws IOException {
+
+    // BufferedInputStream bis;
+    // if (istm instanceof BufferedInputStream) {
+    // bis = (BufferedInputStream) istm;
+    // } else {
+    // bis = new BufferedInputStream(istm);
+    // }
+    // Metadata imageMetadata;
+    // try {
+    // imageMetadata = ImageMetadataReader.readMetadata(bis, true);
+    // } catch (final ImageProcessingException e) {
+    // return;
+    // }
+    // final GpsDirectory gpsDirectory =
+    // imageMetadata.getDirectory(GpsDirectory.class);
+    // if (gpsDirectory == null) {
+    // log.info("Image has no GPS metadata.");
+    // return;
+    // }
+    // final GeoLocation exifLocation = gpsDirectory.getGeoLocation();
+    // if (exifLocation != null) {
+    // final GpxWpt gpxWpt = new GpxWpt();
+    // gpxWpt.wgs = new Wgs(exifLocation.getLatitude(),
+    // exifLocation.getLongitude());
+    // gpxWpt.name =
+    // Iterables.getLast(Arrays.asList(name.split(Pattern.quote(File.separator))),
+    // "");
+    // gpxWpt.link.href = name;
+    // gpxWpt.desc = "EXIF coordinates";
+    // gpxWpt.type = "pic";
+    // gpxWpt.sym = "Photo";
+    // builder.addGpxWpt(gpxWpt);
+    // } else {
+    // log.info("Image {} has GPS metadata, but no lat/lon information.", name);
+    // }
+    // }
 }
